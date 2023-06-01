abbb91ff4d5dfcb3018f831fe2c3997ac5b407e8
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-06-01 09:52:02 -0400

    Se añadio una prueba para validar que devuelva los cambios enteros

--- src/calculadorMonedas.test.js
+++ src/calculadorMonedas.test.js
@@ -9,4 +9,7 @@ describe("Calculador de cantidad minima de monedas para cambio", () => {
     expect(calculadorMonedasMinimas(calculadorCambio(2,2.2))).toEqual("[0.2]");
     expect(calculadorMonedasMinimas(calculadorCambio(2,2.5))).toEqual("[0.5]");
   });
+  it("Se espera recibir una unica moneda de cambio en enteros", () => {
+    expect(calculadorMonedasMinimas(calculadorCambio(2,3))).toEqual("[1]");
+  });
 });
