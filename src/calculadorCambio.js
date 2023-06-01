b9c9b461e0608802da842f0f058f5d27a85fa8a6
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-06-01 09:50:51 -0400

    Se refactorizo la funcion calcular cambio para que considere los cambios decimales y enteros

--- src/calculadorCambio.js
+++ src/calculadorCambio.js
@@ -1,6 +1,10 @@
 function calculadorCambio(montoVenta, efectivo) {
   let cambio = efectivo-montoVenta;
-  return cambio.toFixed(1);
+  let cambioRedondeado=cambio.toFixed(1);
+  let cadenaCambio=cambioRedondeado.toString();
+  if(cadenaCambio[cadenaCambio.length-1]==0) return cambio.toFixed(0);
+  else return cambioRedondeado;
+
 }
 
 export default calculadorCambio;
